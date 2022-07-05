cc49238b0792ac1410d48d3991517a2807eafc31
Тимур <kadyrbekovtimur001@gmail.com>
2022-07-05 17:30:01 +0300

    Initial commit

--- Main.java
+++ Main.java
@@ -1,21 +1,16 @@
-package com.company1;
+package com.company12;
 
-import java.util.Scanner;
+
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Аянт табуу");
         UchBurch uchBurch = new UchBurch();
-        System.out.println("Он жагына сан киргиз");
-        Scanner scanner = new Scanner(System.in);
-        uchBurch.a = scanner.nextDouble();
-        System.out.println("Сол жагына сан киргиз");
-        uchBurch.b = scanner.nextDouble();
-        System.out.println("Узундугуна сан киргиз");
-        uchBurch.c = scanner.nextDouble();
-        System.out.print("Аянты =");
+        uchBurch.a=13;
+        uchBurch.b=14;
+        uchBurch.c=15;
         uchBurch.Area();
+
 
     }
 }
